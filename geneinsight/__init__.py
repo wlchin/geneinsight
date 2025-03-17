dee24c37ae0b1d1f7edd9d6b9efc302579e8a306
Wee Loong Chin <68843088+wlchin@users.noreply.github.com>
2025-03-18 05:54:34 +0800

    Update __init__.py

--- geneinsight/__init__.py
+++ geneinsight/__init__.py
@@ -1,9 +1,6 @@
 """
-TopicGenes: Topic modeling pipeline for gene sets with enrichment analysis.
+GeneInsight: Topic modeling pipeline for gene sets with enrichment analysis.
 """
 
 __version__ = '0.1.0'
 
-from .pipeline import Pipeline
-
-__all__ = ['Pipeline']
